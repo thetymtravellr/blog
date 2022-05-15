49dccd7523e671bfec604506ce415795b3cee02d
Robiul Hasan <muhammadrobiul502@gmail.com>
2022-05-15 21:01:08 +0600

    favicon added

--- src/Components/BlogCard.js
+++ src/Components/BlogCard.js
@@ -1,10 +1,25 @@
-import React from "react";
+import HeartIconActive from "@heroicons/react/outline/HeartIcon";
+import { HeartIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const BlogCard = ({ blog }) => {
 
-  const { article, author, email, react, tag, title, _id } = blog;
+  const [liked, setLiked] = useState(false);
+
+  const { article, author, email, reactions, tag, title, _id } = blog;
   const shortenArticle = article.slice(0, 500);
+ 
+  const likeFunc = () => {
+    setLiked(true)
+    console.log('liked');
+  }
+
+  const disLikedFunc = () => {
+    setLiked(false)
+    console.log('disliked');
+  }
+
 
   return (
     <div className="max-w-lg w-full border mx-auto p-4">
@@ -21,7 +36,18 @@ const BlogCard = ({ blog }) => {
       <div className="mt-4 mb-2">
         <p className="text-justify">{shortenArticle}</p>
       </div>
-      <div></div>
+      <div>
+        <div className="flex items-center space-x-2  w-fit">
+          {
+            liked
+            ?
+            <button className="" onClick={disLikedFunc}><HeartIcon className="w-6 h-6 text-pink-500"/></button>
+            :
+            <button  className="" onClick={likeFunc}><HeartIconActive className="w-6 h-6"/></button>
+          }
+          <div><p className="text-lg text-gray-500 font-hindSiliguri">{reactions}</p></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -3,16 +3,18 @@ import React, { useEffect, useState } from 'react';
 import BlogCard from './BlogCard';
 
 const Blogs = () => {
+    
     const [blogs,setBlogs] = useState([])
     useEffect(()=>{
-         axios.get('http://localhost:8080/blogs')
+         axios.get('https://hasan-blog-app.herokuapp.com/blogs')
          .then(res => setBlogs(res.data))
-    },[])
+    },[blogs])
+    
     return (
         <section className='mt-24'>
             <div className='flex flex-col space-y-2 mx-2'>
                {
-                   blogs.map(blog =>  <BlogCard blog={blog}></BlogCard>)
+                   blogs.map(blog =>  <BlogCard key={blog._id} blog={blog}></BlogCard>)
                }
             </div>
         </section>
